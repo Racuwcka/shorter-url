ae0af6b5f524e017e0b4360b2431842e13701c22
George <13zhora13@gmail.com>
2025-06-04 22:04:30 +0300

    move dir pkg

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-	"github.com/Racuwcka/shorter-url/internal/pkg/handlers"
-	"github.com/Racuwcka/shorter-url/internal/pkg/repositories"
-	"github.com/Racuwcka/shorter-url/internal/pkg/services"
 	"log"
 	"net/http"
+
+	handlers2 "github.com/Racuwcka/shorter-url/pkg/handlers"
+	"github.com/Racuwcka/shorter-url/pkg/repositories"
+	services2 "github.com/Racuwcka/shorter-url/pkg/services"
 )
 
 func main() {
 	repo := repositories.NewShortenCache()
 	baseUrl := "http://localhost:8080"
-	addHandler := handlers.NewAddHandler(services.NewAddService(repo))
+	addHandler := handlers2.NewAddHandler(services2.NewAddService(repo))
 	http.HandleFunc("POST /api/v1/shorten", addHandler.Handle)
 
-	getShortHandler := handlers.NewGetShortHandler(services.NewGetShortService(baseUrl, repo))
+	getShortHandler := handlers2.NewGetShortHandler(services2.NewGetShortService(baseUrl, repo))
 	http.HandleFunc("GET /api/v1/shorten", getShortHandler.Handle)
 
-	getOriginHandler := handlers.NewGetOriginalHandler(services.NewGetOriginalService(baseUrl, repo))
+	getOriginHandler := handlers2.NewGetOriginalHandler(services2.NewGetOriginalService(baseUrl, repo))
 	http.HandleFunc("GET /api/v1/original", getOriginHandler.Handle)
 
-	getHandler := handlers.NewGetHandler(services.NewGetService(repo))
+	getHandler := handlers2.NewGetHandler(services2.NewGetService(repo))
 	http.HandleFunc("GET /link/{shortID}", getHandler.Handle)
 
 	log.Println("Server started on :8080")
